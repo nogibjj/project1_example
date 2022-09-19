112df7de8358593c52fe6d247b03c7ebb184f040
Yitong Wang <90431341+xxwwwww@users.noreply.github.com>
2022-09-18 21:40:51 -0400

    Update fastapi-app.py

--- fastapi-app.py
+++ fastapi-app.py
@@ -10,12 +10,12 @@ async def root():
     return {"message": "Hello Databricks"}
 
 
-@app.get("/add/{num1}/{num2}")
-async def add(num1: int, num2: int):
-    """Add two numbers together"""
+# @app.get("/add/{num1}/{num2}")
+# async def add(num1: int, num2: int):
+#     """Add two numbers together"""
 
-    total = num1 + num2
-    return {"total": total}
+#     total = num1 + num2
+#     return {"total": total}
 
 
 @app.get("/query")
